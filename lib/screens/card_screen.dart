6db1ad5ab2b676e6a79575763ed0e54a29d8db7a
=Matias Salomon <=>
2023-02-16 11:24:03 -0300

    Creacion de Segundo CustomCardType son .gif con argumentos para imagen y texto

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -18,7 +18,25 @@ class CardScreen extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
-          CustomCardType2()
+          CustomCardType2_1(
+            name: 'Paisaje 1',
+            imageUrl:
+                'https://img.freepik.com/free-vector/hand-drawn-flat-design-mountain-landscape_52683-79195.jpg?w=2000',
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CustomCardType2_1(
+            imageUrl:
+                'https://images.squarespace-cdn.com/content/v1/59523d5c4c8b031b6d9dcb5b/1645865436351-NF1WX4AHJUE43OZ3GJCY/_S6A1420-Edit-Edit.jpg?format=2500w',
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CustomCardType2_1(
+            imageUrl:
+                'https://mymodernmet.com/wp/wp-content/uploads/2020/02/international-landscape-photographer-of-the-year-thumbnail.jpg',
+          )
         ],
       ),
     );
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_components/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageUrl;
+
+  const CustomCardType2({super.key, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +15,52 @@ class CustomCardType2 extends StatelessWidget {
       elevation: 10,
       child: Column(
         children: [
-          const FadeInImage(
-            placeholder: AssetImage('assets/jar-loading.gif'),
+          FadeInImage(
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             image: NetworkImage(
-                'https://img.freepik.com/free-vector/hand-drawn-flat-design-mountain-landscape_52683-79195.jpg?w=2000'),
+                imageUrl), //https://img.freepik.com/free-vector/hand-drawn-flat-design-mountain-landscape_52683-79195.jpg?w=2000'
             width: double.infinity,
             height: 300,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 200),
+            fadeInDuration: const Duration(milliseconds: 200),
           ),
           Container(
               alignment: AlignmentDirectional.centerEnd,
               padding: const EdgeInsets.only(bottom: 10, top: 10, right: 15),
               child: const Text('Un Paisaje'))
+        ],
+      ),
+    );
+  }
+}
+
+class CustomCardType2_1 extends StatelessWidget {
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2_1({super.key, required this.imageUrl, this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      shadowColor: AppTheme.primary,
+      elevation: 10,
+      child: Column(
+        children: [
+          Image(
+            image: NetworkImage(
+                imageUrl), //https://img.freepik.com/free-vector/hand-drawn-flat-design-mountain-landscape_52683-79195.jpg?w=2000'
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          if (name != null)
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(bottom: 10, top: 10, right: 15),
+                child: Text(name ?? 'No Title'))
         ],
       ),
     );
