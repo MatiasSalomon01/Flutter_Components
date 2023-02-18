2359f73b0acb44d930de3e2d50a2e2adee388c9a
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-18 15:44:57 -0300

    Centralizacion del TextFormField

--- lib/screens/inputs_screens.dart
+++ lib/screens/inputs_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/widgets/widgets.dart';
 
 class InputScreen extends StatelessWidget {
   const InputScreen({super.key});
@@ -13,31 +14,8 @@ class InputScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  //print(value);
-                },
-                validator: (value) {
-                  if (value == null) return 'Este Campo es Requerido';
-                  return value.length < 3 ? 'Minimo de 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  hintText: 'Nombre del Usuario',
-                  labelText: 'Nombre',
-                  helperText: 'Solo letras',
-                  suffixIcon: Icon(Icons.group_outlined),
-                  // prefixIcon: Icon(Icons.verified_user_outlined)
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  )
-                ),
-              ),
+            children: const [
+               CustomInputField(hintText: 'Nombre del Usuario', helperText: 'Solo 3 letras', labelText: 'Nombre', icon: Icons.assignment_ind_outlined,suffixIcon: Icons.group_outlined),
             ],
           ),
         ),
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -6,18 +6,40 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       //Color primario
       primaryColor: primary,
+
       //Color del AppBar
       appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+
       //Color de Texto en Buttons
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(foregroundColor: primary)),
+
       //Color de Fondo de Boton Flotante
       floatingActionButtonTheme:
           const FloatingActionButtonThemeData(backgroundColor: primary),
+
       //Estilo de elevated button
       elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-              backgroundColor: primary, elevation: 0, shape: StadiumBorder())));
+              backgroundColor: primary, elevation: 0, shape: const StadiumBorder())),
+
+      //Estilo de Input de TextFormField
+      inputDecorationTheme: const InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primary,),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: primary,),
+          borderRadius: BorderRadius.all(Radius.circular(50))
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: primary,),
+          borderRadius: BorderRadius.all(Radius.circular(50))
+        ),
+        border: OutlineInputBorder(
+          //borderSide: BorderSide(color: primary,),
+          borderRadius: BorderRadius.all(Radius.circular(50))
+        ),
+      ),      
+  );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
       //Color primario
