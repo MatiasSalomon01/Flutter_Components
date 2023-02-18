08c379fa0968ecd7add8d88e83e25e731e85c9e4
MatiasSalomon01 <matisalomond@gmail.com>
2023-02-18 15:15:31 -0300

    Decoracion de Input

--- lib/screens/inputs_screens.dart
+++ lib/screens/inputs_screens.dart
@@ -16,11 +16,27 @@ class InputScreen extends StatelessWidget {
             children: [
               TextFormField(
                 autofocus: true,
-                initialValue: 'Matias Salomon',
+                initialValue: '',
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {
-                  print(value);
+                  //print(value);
                 },
+                validator: (value) {
+                  if (value == null) return 'Este Campo es Requerido';
+                  return value.length < 3 ? 'Minimo de 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del Usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',
+                  suffixIcon: Icon(Icons.group_outlined),
+                  // prefixIcon: Icon(Icons.verified_user_outlined)
+                  icon: Icon(Icons.assignment_ind_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  )
+                ),
               ),
             ],
           ),
